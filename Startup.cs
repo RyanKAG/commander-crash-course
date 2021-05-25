db38b46f453053349af295293462c56fd44ed132
ryankag <rayanalgh@gmail.com>
2021-05-25 08:43:46 +0300

    Fixed DbContext and connection with database

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace Commander
 
             services.AddControllers();
 
-            services.AddScoped<ICommanderRepo, MockCommanderRepo>();
+            // services.AddScoped<ICommanderRepo, MockCommanderRepo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
